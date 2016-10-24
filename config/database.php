674bc3533eee7068aff299524f8bb3904c08be84
thiagotmendes <thiagotmendes@outlook.com>
2016-10-24 15:29:36 -0200

    teste

--- config/database.php
+++ config/database.php
@@ -54,11 +54,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'localhost'),
+            'host' => env('DB_HOST', 'sistemapm.mysql.dbaas.com.br'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'sistemapm'),
+            'username' => env('DB_USERNAME', 'sistemapm'),
+            'password' => env('DB_PASSWORD', 'p3quem0ch1l3'),
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
