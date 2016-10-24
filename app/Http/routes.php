6ff052b6ee3b0e4bc77fad7a06dbe4dbeac32721
thiagotmendes <thiagotmendes@outlook.com>
2016-10-24 15:14:56 -0200

    teste

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,16 +1,35 @@
 <?php
-
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
+});
+Route::get('/register', function(){
+  return view('form.register');
+});
+Route::auth();
+Route::get('/home', 'HomeController@index');
+Route::group(['prefix' => '/home'], function(){
+  Route::post('addchave', 'HomeController@insereChave');
+  Route::get('/{id}/{pais?}', 'HomeController@listaJogosPaises');
+});
+// ROTA DOS JOGOS
+Route::get('/executajogo/{idJogos}', 'JogosController@ExecutaJogo');
+// GRUPO DE ROTA PARA USUARIOS
+Route::group(['prefix' => 'addUser'], function () {
+  Route::get('formuser','UsuariosController@index');
+  Route::get('listaUsuarios','UsuariosController@gridUsuario');
+  Route::post('adicionar','UsuariosController@createUser');
+});
+// GRUPO DE ROTAS PARA CONTROLE DE JOGOS
+Route::get('/addjogo', 'JogosController@index');
+Route::group(['prefix' => 'addjogo'], function(){
+  Route::get('listaJogos', 'JogosController@viewJogos');
+  Route::post('/adicionar', 'JogosController@createJogos');
+});
+// GRUPO ROTAS PARA PAIS
+Route::get('addpais', 'PaisController@index');
+Route::group(['prefix' => 'addpais'], function(){
+  Route::get('listaPaises/{idpais}', 'PaisController@updatePais');
+  Route::post('adicionar', 'PaisController@createPais');
+  Route::get('listaPaises', 'PaisController@listaPaises');
+  Route::get('excluir/{idpais}', 'PaisController@excluirPais');
 });
