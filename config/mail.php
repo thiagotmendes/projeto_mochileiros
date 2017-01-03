1a92ba1dc9b5ebc37ba6981e6721abf932df7b4d
Thiago Mendes <thiagotmendes@outlook.com>
2017-01-03 11:30:26 -0200

    atualização de envio de email

--- config/mail.php
+++ config/mail.php
@@ -29,7 +29,7 @@ return [
     |
     */
 
-    'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
+    'host' => env('MAIL_HOST', 'smtp.pequenosmochileiros.com.br'),
 
     /*
     |--------------------------------------------------------------------------
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'from' => ['address' => null, 'name' => null],
+    'from' => ['address' => 'noreply@pequenosmochileiros.com.br', 'name' => 'Pequenos Mochileiros'],
 
     /*
     |--------------------------------------------------------------------------
@@ -81,7 +81,7 @@ return [
     |
     */
 
-    'username' => env('MAIL_USERNAME'),
+    'username' => env('noreply@pequenosmochileiros.com.br'),
 
     /*
     |--------------------------------------------------------------------------
@@ -94,7 +94,7 @@ return [
     |
     */
 
-    'password' => env('MAIL_PASSWORD'),
+    'password' => env('p3quen0m0chil4'),
 
     /*
     |--------------------------------------------------------------------------
